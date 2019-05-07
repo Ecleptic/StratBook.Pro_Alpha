1c6a0a07f60fa1d11049fab34a43c72ea4421216
Cameron Green <cameron@camerongreens.com>
2019-05-07 14:12:58 -0500

    removed all stratedit data. gotta start this from scratch again

--- frontend/components/OwStrategy/StratEdit.js
+++ frontend/components/OwStrategy/StratEdit.js
@@ -10,13 +10,7 @@ import Signup from '../Signup_Portal'
 // import TextEditor from './textEditor'
 import User from '../User'
 
-import {
-	OwHeroes,
-	OwMaps,
-	OwControlMapsData,
-	OwMapTypes,
-	OwMapToEnum
-} from '../../configs/Overwatch/OwData'
+import { OwHeroes, OwMaps, OwControlMapsData, OwMapTypes, OwMapToEnum } from '../../configs/Overwatch/OwData'
 
 const CREATE_OW_STRATEGY_MUTATION = gql`
 	mutation CREATE_OW_STRATEGY_MUTATION(
@@ -51,14 +45,7 @@ const CREATE_OW_STRATEGY_MUTATION = gql`
 `
 const GET_STRATEGIES_QUERY = gql`
 	query GET_STRATEGIES_QUERY($userName: String!, $mapName: OwMap!) {
-		owStrategies(
-			where: {
-				AND: [
-					{ creatorName: { name: $userName } }
-					{ mapName: $mapName }
-				]
-			}
-		) {
+		owStrategies(where: { AND: [{ creatorName: { name: $userName } }, { mapName: $mapName }] }) {
 			id
 			mapName
 			defenseStrats
@@ -78,34 +65,6 @@ const GET_STRATEGIES_QUERY = gql`
 `
 
 class StratEdit extends Component {
-	// state = {
-	//     mapName: this.props.mapName || '',
-	//     defenseStrats:
-	//         '"# Counter Dive\n1. Winston dives first, targets the primary squish support first (zen, ana, etc.)\n2. DVA follows closely behind and matrixes the winston on dive.\n3. Genji Dives with the tanks.\n4. Lucio might follow...\n5. Sombra, Ana, & Lucio deal with the counterdive.\n  1. Kill the main diver (probably winston)\n  2. Follow up with the rest of the counter dive.\n6. **WIN**\n',
-	//     defenseHeroes: [
-	//         'Zenyatta',
-	//         'Mei',
-	//         'Reinhardt',
-	//         'Ana',
-	//         'Dva',
-	//         'Widowmaker'
-	//     ],
-	//     offenseStrats:
-	//         '"# Counter Dive\n1. Winston dives first, targets the primary squish support first (zen, ana, etc.)\n2. DVA follows closely behind and matrixes the winston on dive.\n3. Genji Dives with the tanks.\n4. Lucio might follow...\n5. Sombra, Ana, & Lucio deal with the counterdive.\n  1. Kill the main diver (probably winston)\n  2. Follow up with the rest of the counter dive.\n6. **WIN**\n',
-	//     offenseHeroes: [
-	//         'Zenyatta',
-	//         'Mei',
-	//         'Reinhardt',
-	//         'Ana',
-	//         'Dva',
-	//         'Widowmaker'
-	//     ],
-	//     mapMode: '',
-	//     strategyName: 'Divess',
-	//     expectedRank: '', //TODO: might give an error later
-	//     // subMap: this.props.subMap || '',
-	//     creatorName: 'ecleptic'
-	// }
 	state = {
 		mapName: '',
 		defenseStrats: '',
@@ -184,233 +143,31 @@ class StratEdit extends Component {
 						)
 					} else {
 						return (
-                            <Query
-                                query={GET_STRATEGIES_QUERY}
-                                variables={{ userName, mapName: OwMapToEnum(mapName) }}
-                            >
-                                {({ data, loading, error, userName }) => {
-                                    if (loading) return 'Loading'
-                                    if (error) return <p>Error: {`${error}`}</p>
-                                    const {
-                                        id,
-                                        // mapName,
-                                        defenseStrats,
-                                        defenseHeroes,
-                                        offenseStrats,
-                                        offenseHeroes,
-                                        mapMode,
-                                        strategyName,
-                                        expectedRank,
-                                        subMap,
-                                        creatorName
-                                    } = data.owStrategies[0]
+							<Query query={GET_STRATEGIES_QUERY} variables={{ userName, mapName: OwMapToEnum(mapName) }}>
+								{({ data, loading, error, userName }) => {
+									if (loading) return 'Loading'
+									if (error) return <p>Error: {`${error}`}</p>
+									const {
+										id,
+										// mapName,
+										defenseStrats,
+										defenseHeroes,
+										offenseStrats,
+										offenseHeroes,
+										mapMode,
+										strategyName,
+										expectedRank,
+										subMap,
+										creatorName
+									} = data.owStrategies[0]
 
-                                    return (
-							<Mutation
-								mutation={CREATE_OW_STRATEGY_MUTATION}
-								variables={this.state}
-							>
-								{(createOwStrategy, { loading, error }) => (
-									<form
-										data-test="form"
-										onSubmit={async e => {
-											// 1. Stop the form from submitting
-											e.preventDefault()
-											console.log(
-												'SUBMITTING FORM:',
-												this.state
-											)
-											// 2. call the mutation
-											try {
-												const res = await createOwStrategy().then(
-													response => {
-														console.log(response)
-													}
-												)
-												console.log('SUCCESS', res)
-											} catch (error) {
-												console.error(
-													'dang it broke:',
-													error
-												)
-											}
-											// 3. change the page the view
-										}}
-									>
-										<Error error={error} />
-										<fieldset
-											disabled={loading}
-											aria-busy={loading}
-										>
-											<label htmlFor="creatorName">
-												creatorName
-												<br />
-												<textarea
-													id="creatorName"
-													name="creatorName"
-													placeholder="Enter a creator Name"
-													required
-													value={
-														this.state.creatorName
-													}
-													onChange={this.handleChange}
-												/>
-											</label>
-											<br />
-											<label htmlFor="strategyName">
-												Strategy Name
-												{/* <br /> */}
-												<input
-													id="strategyName"
-													name="strategyName"
-													placeholder="Name your Strategy"
-													required
-													type="text"
-													value={
-														this.state.strategyName
-													}
-													onChange={this.handleChange}
-												/>
-											</label>
-											<br />
-											{/*TODO: we probably want react-select for images https://jedwatson.github.io/react-select/
-                                            https://github.com/JedWatson/react-select/issues/2553*/}
-											<label htmlFor="ExpectedRankSelect">
-												<select
-													name="expectedRank"
-													id="expectedRankSelect"
-													onChange={this.handleChange}
-												>
-													<option />
-													{[
-														'Bronze',
-														'Silver',
-														'Gold',
-														'Platinum',
-														'Diamond',
-														'Master',
-														'Grand Master',
-														'Top 500'
-													].map(rank => (
-														<option key={rank}>
-															{rank}
-														</option>
-													))}
-												</select>
-											</label>
-
-											<br />
-
-											<label htmlFor="offenseHeroes">
-												offenseHeroes
-												<ul>
-													{[0, 1, 2, 3, 4, 5].map(
-														index => {
-															return (
-																<li key={index}>
-																	<select
-																		name={`offenseHeroes`}
-																		id={`offenseHeroes${index}`}
-																		data-index={
-																			index
-																		}
-																		// selected={index}
-																		onChange={
-																			this
-																				.handleSelectChange
-																		}
-																	>
-																		<option />
-																		{OwHeroes.map(
-																			hero => (
-																				<option
-																					key={
-																						hero
-																					}
-																				>
-																					{
-																						hero
-																					}
-																				</option>
-																			)
-																		)}
-																	</select>
-																</li>
-															)
-														}
-													)}
-												</ul>
-											</label>
-											<DraftEditor
-												updateMD={this.updateMD}
-												isDefense={false}
-											/>
-											{/*
-                                            <TextEditor
-                                                updateMD={this.updateMD}
-                                                isDefense={false}
-                                            /> */}
-
-											<label htmlFor="defenseHeroes">
-												defenseHeroes
-												<ul>
-													{[0, 1, 2, 3, 4, 5].map(
-														index => {
-															return (
-																<li key={index}>
-																	<select
-																		name={`defenseHeroes`}
-																		id={`defenseHeroes${index}`}
-																		data-index={
-																			index
-																		}
-																		onChange={
-																			this
-																				.handleSelectChange
-																		}
-																		// selected={index}
-																	>
-																		<option />
-																		{OwHeroes.map(
-																			hero => (
-																				<option
-																					key={
-																						hero
-																					}
-																				>
-																					{
-																						hero
-																					}
-																				</option>
-																			)
-																		)}
-																	</select>
-																</li>
-															)
-														}
-													)}
-												</ul>
-											</label>
-
-											{/* <TextEditor
-                                                updateMD={this.updateMD}
-                                                isDefense={true}
-                                            /> */}
-											<DraftEditor
-												updateMD={this.updateMD}
-												isDefense={true}
-											/>
-
-											<button type="submit">
-												Submit
-											</button>
-										</fieldset>
-									</form>
-								)}
-							</Mutation>
-                                    )
-                                }}
-                            </Query>
+									return (
+										<Mutation mutation={CREATE_OW_STRATEGY_MUTATION} variables={this.state}>
+											{(createOwStrategy, { loading, error }) => <></>}
+										</Mutation>
+									)
+								}}
+							</Query>
 						)
 					}
 				}}
