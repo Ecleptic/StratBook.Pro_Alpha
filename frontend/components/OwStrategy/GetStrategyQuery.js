acfa12e233c5d185ccebf2a0f3f7f17c9c5da052
= <cameron@camerongreens.com>
2019-05-30 15:09:15 -0500

    i don't understand my own code anymore

--- frontend/components/OwStrategy/GetStrategyQuery.js
+++ frontend/components/OwStrategy/GetStrategyQuery.js
@@ -6,15 +6,14 @@ import { Query } from 'react-apollo'
 // import HeroImage from './HeroImage'
 import { OwHeroes } from '../../configs/Overwatch/OwData'
 
-
-
 const StratView = props => (
-	<Query {...props} query={GET_STRATEGIES_QUERY}>
-		{payload => props.children(payload)}
-	</Query>
+	// <Query {...props} query={GET_STRATEGIES_QUERY}>
+	// 	{payload => props.children(payload)}
+	// </Query>
+	<></>
 )
 
 StratView.propTypes = {}
 
 export default StratView
-export { GET_STRATEGIES_QUERY }
+// export { GET_STRATEGIES_QUERY }
